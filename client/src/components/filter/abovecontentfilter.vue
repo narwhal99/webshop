f55b76c51e65e259ae93061af21eef05dea5be43
narwhal99 <molnarandre@outlook.com>
2020-10-27 12:56:17 +0100

    commit before upgrade

---
+++ client/src/components/filter/abovecontentfilter.vue
@@ -0,0 +1,19 @@
+<template>
+  <container>
+    <v-row>
+      <v-col>
+        <v-select></v-select>
+      </v-col>
+      <v-col>
+        <v-select></v-select>
+      </v-col>
+      <v-col>
+        <v-select></v-select>
+      </v-col>
+    </v-row>
+  </container>
+</template>
+
+<script>
+export default {};
+</script>
--- client/src/views/contentDisplay.vue
+++ client/src/views/contentDisplay.vue
@@ -1,19 +1,35 @@
 <template>
-  <v-row justify="center" color="black">
-    <template v-for="group in getContent">
-      <v-col sm="6" md="3" v-for="product in group.product" :key="product._id">
-        <productCard :group_name="group.name" :group_price="group.price" :product="product" />
-      </v-col>
-    </template>
-  </v-row>
+  <v-container>
+    <v-row justify="start">
+      <filters />
+    </v-row>
+    <v-row justify="center" color="black">
+      <template v-for="group in getContent">
+        <v-col
+          sm="6"
+          md="3"
+          v-for="product in group.product"
+          :key="product._id"
+        >
+          <productCard
+            :group_name="group.name"
+            :group_price="group.price"
+            :product="product"
+          />
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import productCard from "../components/cards/product";
+import filters from "../components/filter/abovecontentfilter";
 export default {
   components: {
     productCard,
+    filters,
   },
   watch: {
     $route(to) {
